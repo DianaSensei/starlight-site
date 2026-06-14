7a0c9f51b5b8766ab91b23bd7f60a85c2ba114d0
Nguyễn Đức Thông <43933486+DianaSensei@users.noreply.github.com>
2026-06-15 00:39:19 +0700

    fix: ci build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://dianasensei.github.io',
   base: '/devtool-site',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
 });
